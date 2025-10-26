f7119bffad388f7cb3b15e3b2b10fcec0fcfe515
Jose Acena <joseracena@gmail.com>
2025-10-26 11:21:39 +0000

    added a variety of tests for get()

--- lab1/test_clientserver.py
+++ lab1/test_clientserver.py
@@ -1,10 +1,12 @@
 """
 Simple client server unit test
 """
-
+import textwrap
 import logging
 import threading
 import unittest
+import io
+from contextlib import redirect_stdout
 
 import clientserver
 from context import lab_logging
@@ -15,7 +17,7 @@ lab_logging.setup(stream_level=logging.INFO)
 class TestEchoService(unittest.TestCase):
     """The test"""
     _server = clientserver.Server()  # create single server in class variable
-    _server_thread = threading.Thread(target=_server.serve)  # define thread for running server
+    _server_thread = threading.Thread(target=_server.handler)  # define thread for running server
 
     @classmethod
     def setUpClass(cls):
@@ -25,10 +27,76 @@ class TestEchoService(unittest.TestCase):
         super().setUp()
         self.client = clientserver.Client()  # create new client for each test
 
-    def test_srv_get(self):  # each test_* function is a test
-        """Test simple call"""
-        msg = self.client.call("Hello VS2Lab")
-        self.assertEqual(msg, 'Hello VS2Lab*')
+    # def test_srv_get(self):  # each test_* function is a test
+    #     """Test simple call"""
+    #     msg = self.client.call("Hello VS2Lab")
+    #     self.assertEqual(msg, 'Hello VS2Lab*')
+
+    def test_simple_get(self): 
+        msg = self.client.get("Alice")
+        self.assertEqual(msg, "Alice : +49 671 6367577")
+
+    def test_capslock_get(self): 
+        msg = self.client.get("CARLOS")
+        self.assertEqual(msg, "Carlos : +49 957 0269783")
+
+    def test_number_get(self): 
+        f = io.StringIO()
+        with redirect_stdout(f): 
+            msg = self.client.get(32)
+        output = f.getvalue().strip()
+        self.assertIn("This is an invalid request --> '32'", output)
+        self.assertIn("Please use only letters from the alphabet!", output)
+        self.assertIsNone(msg) # no response cuz invalid input
+         
+    def test_numberAsString_get(self): 
+        f = io.StringIO()
+        with redirect_stdout(f): 
+            msg = self.client.get("9")
+        output = f.getvalue().strip()
+        self.assertIn("This is an invalid request --> '9'", output)
+        self.assertIn("Please use only letters from the alphabet!", output)
+        self.assertIsNone(msg) # no response cuz invalid input
+   
+    def test_mixInvalidInput_get(self): 
+        f = io.StringIO()
+        with redirect_stdout(f): 
+            msg = self.client.get("22BoB22")
+        output = f.getvalue().strip()
+        self.assertIn("This is an invalid request --> '22BoB22'", output)
+        self.assertIn("Please use only letters from the alphabet!", output)
+        self.assertIsNone(msg) # no response cuz invalid input
+   
+    def test_numberInString_get(self): 
+        f = io.StringIO()
+        with redirect_stdout(f): 
+            msg = self.client.get("R0bert")
+        output = f.getvalue().strip()
+        self.assertIn("This is an invalid request --> 'R0bert'", output)
+        self.assertIn("Please use only letters from the alphabet!", output)
+        self.assertIsNone(msg) # no response cuz invalid input
+    
+    def test_void_get(self):
+        f = io.StringIO()
+        with redirect_stdout(f): 
+            msg = self.client.get()
+        output = f.getvalue().strip()
+        self.assertIn("This is an invalid request --> ''", output)
+        self.assertIn("Please use only letters from the alphabet!", output)
+        self.assertIsNone(msg) # no response cuz invalid input
+
+    def test_empty_get(self):
+        f = io.StringIO()
+        with redirect_stdout(f): 
+            msg = self.client.get("")
+        output = f.getvalue().strip()
+        self.assertIn("This is an invalid request --> ''", output)
+        self.assertIn("Please use only letters from the alphabet!", output)
+        self.assertIsNone(msg) # no response cuz invalid input
+
+    def test_inexistentKey_get(self):
+        msg = self.client.get("Cooper")
+        self.assertEqual(msg, "Cooper : no entry found")
 
     def tearDown(self):
         self.client.close()  # terminate client after each test
