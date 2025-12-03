790166c875da01ef6b5027df7d0579350f7ddb5c
Jose Acena <joseracena@gmail.com>
2025-12-03 13:20:11 +0000

    initial state of files

---
+++ lab3/zmq3/mapper.py
@@ -0,0 +1,50 @@
+import pickle
+import sys
+import zmq
+
+import constPipe
+
+address_pull = "tcp://" + constPipe.SRC + ":" + constPipe.PORT1  # 1st task src
+address_push_1 = "tcp://" + constPipe.SRC + ":" + constPipe.PORT2  # how and where to connect
+address_push_2 = "tcp://" + constPipe.SRC + ":" + constPipe.PORT3  # how and where to connect
+
+me = str(sys.argv[1])
+context = zmq.Context()
+pull_socket = context.socket(zmq.PULL)  # create a pull socket
+pull_socket.connect(address_pull)  # connect to task source
+
+push_socket_1 = context.socket(zmq.PUSH)  # create a push socket
+push_socket_1.bind(address_push_1)  # bind socket to address
+
+push_socket_2 = context.socket(zmq.PUSH)  # create a push socket
+push_socket_2.bind(address_push_2)  # bind socket to address
+
+
+
+print(f"{me} started")
+
+while True:
+    work = pickle.loads(pull_socket.recv())  # receive work from a source
+    print("{} received workload '{}' from {}".format(me, work[1], work[0]))
+    words = work[1].strip().split(' ')
+
+    # Hash word
+    # word mod reducer.count
+    # send to reducer
+
+    for word in words:
+        if not word: 
+            continue
+
+        reducer_id = hash(word) % 2
+        if reducer_id > 0:
+            print(f"push {reducer_id}:", word)
+            #push_socket_2.send(pickle.dumps((me, word)))
+        else:
+            print(f"push {reducer_id}:", word)
+            #push_socket_1.send(pickle.dumps((me, word)))
+
+
+
+
+    
---
+++ lab3/zmq3/splitter.py
@@ -0,0 +1,27 @@
+import pickle
+
+import zmq
+import constPipe
+
+src = constPipe.SRC
+prt = constPipe.PORT1
+
+with open("Input.txt") as f:
+    t_raw = f.read()
+
+me = "splitter"
+
+t_cleaned_up = ''.join(t_raw.splitlines()).replace(',', '')
+
+t_sentence_splitted = t_cleaned_up.split('.')
+
+context = zmq.Context()
+push_socket = context.socket(zmq.PUSH)  # create a push socket
+
+
+address = "tcp://" + src + ":" + prt  # how and where to connect
+push_socket.bind(address)  # bind socket to address
+
+for sentence in t_sentence_splitted:
+    push_socket.send(pickle.dumps((me, sentence)))
+    print("pushed:", sentence)
