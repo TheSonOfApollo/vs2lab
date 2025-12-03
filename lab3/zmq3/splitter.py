37157a9642b27baa202989b7e98338cfc982f518
Jose Acena <joseracena@gmail.com>
2025-12-03 20:13:09 +0000

    fixed round robin by delaying splitter

--- lab3/zmq3/splitter.py
+++ lab3/zmq3/splitter.py
@@ -1,12 +1,12 @@
 import pickle
-
+import time
 import zmq
 import constPipe
 
 src = constPipe.SRC
 prt = constPipe.SPLITTER_PORT
 
-with open("Input.txt") as f:
+with open("input2.txt") as f:
     t_raw = f.read()
 
 me = "splitter"
@@ -22,6 +22,8 @@ push_socket = context.socket(zmq.PUSH)  # create a push socket
 address = "tcp://" + src + ":" + prt  # how and where to connect
 push_socket.bind(address)  # bind socket to address
 
+time.sleep(0.5) # gives enough time for mappers to react and connect recv()
 for sentence in t_sentence_splitted:
+    # time.sleep(0.5) # to see the splitter and mappers at work 
     push_socket.send(pickle.dumps((me, sentence)))
     print("pushed:", sentence)
